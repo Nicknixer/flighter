01f354131c90ed0bdb2ecc0c0f80187a0ceb921c
Nick Sergeychuk <nicknixer@gmail.com>
2017-02-18 22:11:03 +0300

    fix: rebalance

--- app.js
+++ app.js
@@ -14,7 +14,22 @@ document.onkeydown = key;
 class Statistic {
     constructor() {
         this.money = 0;
-        this.drawMoney();
+        this.health = 100;
+        this.shipLevel = 1;
+        this.weaponLevel = 1;
+        this.level = 0;
+
+        this.style = new PIXI.TextStyle({
+            fontFamily: 'Arial',
+            fontSize: 16,
+            fill: '#000'
+        });
+        this.moneyView = new PIXI.Text('', this.style);
+        this.moneyView.x = 10;
+        this.moneyView.y = 20;
+        app.stage.addChild(this.moneyView);
+
+        this.drawStat();
     }
 
     /*
@@ -24,46 +39,71 @@ class Statistic {
 
     addMoney(arg) {
         this.money += arg;
-        this.moneyView.text =
-            "Деньги: " + Math.floor(this.money) +
-            "\nУровень оружия: " + ship.weaponLevel +
-            "\nСтоимость улучшения: " + this.getCostOfNewUpgrade() + " (Нажмите 1 для улучшения)";
+        this.drawStat();
+    }
+
+    addHealth(arg) {
+        this.health += arg;
+        this.drawStat();
     }
 
     getCostOfNewUpgrade() {
-        return ship.weaponLevel*40 + 10;
+        return this.weaponLevel*this.weaponLevel*30;
+    }
+
+    getCostOfNewUpgradeShip() {
+        return this.shipLevel*this.shipLevel*30;
     }
 
     upgradeWeapon() {
         let cost = this.getCostOfNewUpgrade();
+        if(this.money > cost) {
+            this.addMoney(-cost);
+            this.weaponLevel += 1;
+        }
+        this.drawStat();
+    }
+
+    upgradeShip() {
+        let cost = this.getCostOfNewUpgradeShip();
         if(stats.money > cost) {
             stats.addMoney(-cost);
-            ship.weaponLevel += 1;
+            this.shipLevel += 1;
         }
+        this.drawStat();
     }
 
     upgradeAllWeapon(){
-        ship.weaponLevel += 10;
+        this.weaponLevel += 10;
+        this.drawStat();
+    }
+
+    upgradeAllShip(){
+        this.shipLevel += 10;
+        this.drawStat();
     }
 
     /*
      * Отрисовка количества денег
      */
 
-    drawMoney() {
-        var style = new PIXI.TextStyle({
-            fontFamily: 'Arial',
-            fontSize: 16,
-            fill: '#000'
-        });
-        this.moneyView = new PIXI.Text('', style);
+    drawStat() {
         this.moneyView.text =
-            "Деньги: " + 0 +
-            "\nУровень оружия: " + 0 +
-            "\nСтоимость улучшения: " + 10 + " (Нажмите 1 для улучшения)";
-        this.moneyView.x = 0;
-        this.moneyView.y = 0;
-        app.stage.addChild(this.moneyView);
+            "---УЛУЧШЕНИЯ---" +
+            "\nОружие: " + this.getCostOfNewUpgrade() + " (Нажмите 1)" +
+            "\nКорабль: " + this.getCostOfNewUpgradeShip() + " (Нажмите 2)" +
+            "\n\n---ИГРОК---" +
+            "\nДеньги: " + Math.floor(this.money) +
+            "\nЗдоровье:" + Math.floor(this.health) + "/" + this.shipLevel*100;
+    }
+
+    healShip() {
+        this.level += 0.07;
+        if(this.health >= this.shipLevel*100) {
+            this.health = this.shipLevel*100;
+        } else {
+            this.addHealth(this.shipLevel*3);
+        }
     }
 
     /*
@@ -71,7 +111,12 @@ class Statistic {
      */
 
     clear() {
-        this.addMoney(-1 * this.money);
+        this.money = 0;
+        this.health = 100;
+        this.shipLevel = 1;
+        this.weaponLevel = 1;
+        this.level = 1;
+        this.money = 0;
     }
 }
 
@@ -116,8 +161,14 @@ function startTheGame() {
             meteor.y += meteor.verticalSpeed;
             meteor.x += meteor.horizontalSpeed;
             if(ship.isHit(meteor.x, meteor.y)) {
-                app.stage.removeChild(ship);
-                gameOver();
+                stats.addHealth(-meteor.health);
+                stats.addMoney(meteor.width/7);
+                app.stage.removeChild(meteor);
+                meteors.splice(i, 1);
+                if(stats.health <= 0) {
+                    app.stage.removeChild(ship);
+                    gameOver();
+                }
             }
 
             // Удаляем метеоры, вылетевшие за экран
@@ -148,7 +199,7 @@ function startTheGame() {
                     meteor.health -= bullet.damage;
                     bullet.y = -11; // Уничтожаем пулю
                     if(meteor.health <= 0) {
-                        stats.addMoney(meteor.width/10);
+                        stats.addMoney(meteor.width/4);
                         app.stage.removeChild(meteor);
                         meteors.splice(i, 1);
                     }
@@ -162,8 +213,8 @@ function startTheGame() {
         });
 
         // Двигаем корабль
-        ship.x += (x - ship.x) * 0.02;
-        ship.y += (y - ship.y) * 0.02;
+        ship.x += (x - ship.x) * ( (stats.shipLevel/10+1) / 100 );
+        ship.y += (y - ship.y) * ( (stats.shipLevel/10+1) / 100 );
 
         if(ship.y > app.renderer.height - 20) {
             ship.y = app.renderer.height - 20;
@@ -197,11 +248,11 @@ function getRand(min, max) {
 function fire(e) {
     if(ship) {
         let d = new Date();
-        if(d.getTime() - lastShoot > 1200) {
+        if(d.getTime() - lastShoot > 800 - (stats.weaponLevel*15)) {
 
             let horizontalSpeedLeftAndRightWeapon = 0.4;
 
-            if(ship.weaponLevel >= 0) {
+            if(stats.weaponLevel > 0) {
                 let bullet = PIXI.Sprite.fromImage('sprites/rocket.png');
                 bullet.anchor.set(0.5);
                 bullet.width = 20;
@@ -216,7 +267,7 @@ function fire(e) {
                 app.stage.addChild(bullet);
             }
 
-            if(ship.weaponLevel > 0) {
+            if(stats.weaponLevel > 1) {
                 let bullet = PIXI.Sprite.fromImage('sprites/rocket.png');
                 bullet.anchor.set(0.5);
                 bullet.width = 15;
@@ -231,7 +282,7 @@ function fire(e) {
                 app.stage.addChild(bullet);
             }
 
-            if(ship.weaponLevel > 0) {
+            if(stats.weaponLevel > 2) {
                 let bullet = PIXI.Sprite.fromImage('sprites/rocket.png');
                 bullet.anchor.set(0.5);
                 bullet.width = 15;
@@ -246,7 +297,7 @@ function fire(e) {
                 app.stage.addChild(bullet);
             }
 
-            if(ship.weaponLevel > 1) {
+            if(stats.weaponLevel > 3) {
                 let bullet = PIXI.Sprite.fromImage('sprites/smallrocket.png');
                 bullet.anchor.set(0.5);
                 bullet.width = 10;
@@ -260,7 +311,7 @@ function fire(e) {
                 bullets.push(bullet);
                 app.stage.addChild(bullet);
             }
-            if(ship.weaponLevel > 1) {
+            if(stats.weaponLevel > 4) {
                 let bullet = PIXI.Sprite.fromImage('sprites/smallrocket.png');
                 bullet.anchor.set(0.5);
                 bullet.width = 10;
@@ -275,7 +326,7 @@ function fire(e) {
                 app.stage.addChild(bullet);
             }
 
-            if(ship.weaponLevel > 2) {
+            if(stats.weaponLevel > 5) {
                 let bullet = PIXI.Sprite.fromImage('sprites/rocket.png');
                 bullet.anchor.set(0.5);
                 bullet.width = 20;
@@ -290,7 +341,7 @@ function fire(e) {
                 app.stage.addChild(bullet);
             }
 
-            if(ship.weaponLevel > 3) {
+            if(stats.weaponLevel > 6) {
                 let bullet = PIXI.Sprite.fromImage('sprites/rocket.png');
                 bullet.anchor.set(0.5);
                 bullet.width = 20;
@@ -305,7 +356,37 @@ function fire(e) {
                 app.stage.addChild(bullet);
             }
 
-            if(ship.weaponLevel > 4) {
+            if(stats.weaponLevel > 7) {
+                let bullet = PIXI.Sprite.fromImage('sprites/rocket.png');
+                bullet.anchor.set(0.5);
+                bullet.width = 15;
+                bullet.height = 15;
+                bullet.horizontalSpeed = -horizontalSpeedLeftAndRightWeapon*3;
+                bullet.x = ship.x - 10;
+                bullet.y = ship.y+20;
+                bullet.verticalSpeed = -1;
+                bullet.a = 1.02;
+                bullet.damage = bullet.width+bullet.height+30;
+                bullets.push(bullet);
+                app.stage.addChild(bullet);
+            }
+
+            if(stats.weaponLevel > 8) {
+                let bullet = PIXI.Sprite.fromImage('sprites/rocket.png');
+                bullet.anchor.set(0.5);
+                bullet.width = 15;
+                bullet.height = 15;
+                bullet.horizontalSpeed = horizontalSpeedLeftAndRightWeapon*3;
+                bullet.x = ship.x + 10;
+                bullet.y = ship.y+20;
+                bullet.verticalSpeed = - 1;
+                bullet.a = 1.02;
+                bullet.damage = bullet.width+bullet.height+30;
+                bullets.push(bullet);
+                app.stage.addChild(bullet);
+            }
+
+            if(stats.weaponLevel > 9) {
                 let bullet = PIXI.Sprite.fromImage('sprites/smallrocket.png');
                 bullet.anchor.set(0.5);
                 bullet.width = 30;
@@ -337,7 +418,9 @@ function createShip() {
     ship.height = 110;
     ship.isHit = isHit;
     ship.coins = 0;
-    ship.weaponLevel = 0;
+    ship.health = 100;
+    ship.shipLevel = 1;
+    ship.weaponLevel = 1;
     app.stage.addChild(ship);
 }
 
@@ -365,26 +448,31 @@ function createCloud() {
  */
 
 function createMeteor() {
-    let meteor = PIXI.Sprite.fromImage('sprites/meteor.png');
-    meteor.anchor.set(0.5);
-    meteor.x = getRand(-10, app.renderer.width +10);
-    meteor.y = -400;
-    meteor.scaleArg = getRand(-5, 5) / 200;
-    if(meteor.x > app.renderer.width/2) {
-        meteor.horizontalSpeed = -1*getRand(0,10)/20;
-    } else {
-        meteor.horizontalSpeed = getRand(0,10)/20;
+    for(let i = 0; i < stats.level; i++) {
+        let meteor = PIXI.Sprite.fromImage('sprites/meteor.png');
+        meteor.anchor.set(0.5);
+        meteor.x = getRand(-50, app.renderer.width +50);
+        meteor.y = -400;
+        meteor.scaleArg = getRand(-5, 5) / 200;
+        if(meteor.x > app.renderer.width/2) {
+            meteor.horizontalSpeed = -1*getRand(0,25)/20;
+        } else {
+            meteor.horizontalSpeed = getRand(0,25)/20;
+        }
+        meteor.verticalSpeed = getRand(10+stats.level,10+stats.level*3)/10;
+        meteor.width = 30/meteor.verticalSpeed + 20 + stats.level;
+        meteor.height = 25/meteor.verticalSpeed + 20 + stats.level;
+        //meteor.scale.x *= 0.1;
+        //meteor.scale.y *= 0.1;
+        meteor.isHit = isHit;
+        meteor.health = meteor.width*meteor.height/50;
+        console.log(meteor.health);
+        meteors.push(meteor);
+        app.stage.addChild(meteor);
     }
-    meteor.verticalSpeed = getRand(10,20)/10;
-    meteor.width = 50/meteor.verticalSpeed + 10;
-    meteor.height = 40/meteor.verticalSpeed + 10;
-    //meteor.scale.x *= 0.1;
-    //meteor.scale.y *= 0.1;
-    meteor.isHit = isHit;
-    meteor.health = meteor.width+meteor.height-10;
-    console.log(meteor.health);
-    meteors.push(meteor);
-    app.stage.addChild(meteor);
+
+
+    stats.healShip();
 }
 
 /*
@@ -422,7 +510,7 @@ function gameOver() {
 
 function start() {
     createShip();
-    intervalID.push(setInterval(createMeteor, 1000));
+    intervalID.push(setInterval(createMeteor, 3000));
     intervalID.push(setInterval(createCloud, 2000));
 }
 
@@ -478,19 +566,17 @@ function showInterface() {
     app.stage.addChild(interfaceLabel);
 }
 
-function upgradeWeapon() {
-    let cost = ship.weaponLevel*10 + 10;
-    if(stats.money > cost) {
-        stats.addMoney(-cost);
-        ship.weaponLevel += 1;
-    }
-}
-
 function key(e) {
     if(event.keyCode == 49) {
         stats.upgradeWeapon();
     }
+
+    if(event.keyCode == 50) {
+        stats.upgradeShip();
+    }
+
     if(event.keyCode == 55) {
         stats.upgradeAllWeapon();
+        stats.upgradeAllShip();
     }
 }
